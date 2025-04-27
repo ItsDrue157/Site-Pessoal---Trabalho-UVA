7743ca5229f263ff024d12e1778398b6b25a9fe7
Carlos <carlosmateus7720@gmail.com>
2025-04-27 17:40:20 -0300

    add func forms

--- app.js
+++ app.js
@@ -12,22 +12,38 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.assign('index.html');
         }
     }
-    // Formulário
-    const submitButton = document.querySelector('.submitButton')
-    console.log(submitButton)
-    submitButton.addEventListener('click', () => {
+
+
+
+        // formualrio de contato
+        const submitButton = document.querySelector('.submitButton');submitButton.addEventListener('click', () => {
         const FirstName = document.getElementById("pname").value;
         const escolha_dev = document.getElementById("escolha_dev").value;
         const LastName = document.getElementById("uname").value;
         const Select = document.getElementById("Assunto").value;
         const Message = document.getElementById("Mensagem").value;
-        if(FirstName && LastName && Select !== "none"){
-            submitButton.setAttribute('href', `mailto:uva@veigadealmeida.edu.br?subject=${FirstName} ${LastName} - ${Select}&body=Olá, meu nome é ${FirstName} ${LastName} e gostaria de falar sobre ${Select}. \n Mensagem: ${Message} `);
-            submitButton.setAttribute('target', '_blank');
+    
+        // Mapeamento de destinatários
+        const devEmails = {
+            gabrielRamos: "gabriel.r.silva2806@gmail.com",
+            gabrielFelix: "gabriel.barros@veigadealmeida.edu.br",
+            carlos: "17573508700@veigadealmeida.edu.br",
+            miguelLuis: "miguel.luis@example.com",
+            joaoItalo: "joao.italo@example.com"
+        };
+    
+        if (FirstName && LastName && Select !== "none" && escolha_dev !== "none") {
+            const devEmail = devEmails[escolha_dev];
+            if (devEmail) {
+                submitButton.setAttribute('href', `mailto:${devEmail}?subject=${FirstName} ${LastName} - ${Select}&body=Olá, meu nome é ${FirstName} ${LastName} e gostaria de falar sobre ${Select}. \n Mensagem: ${Message}`);
+                submitButton.setAttribute('target', '_blank');
+            } else {
+                alert("O destinatário selecionado não é válido.");
+            }
         } else {
             alert("Preencha todos os campos obrigatórios");
         }
-    })
+    });
 
     // função para contar os caracteres do textarea
     const textArea = document.getElementById("Mensagem");
